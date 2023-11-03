b04b61faa77eeca750a198fb5f94cb90638a6f29
Yazan-Ksibeh <yazantkas@gmail.com>
2023-11-03 22:29:49 +0200

    first commit

---
+++ src/components/Main.svelte
@@ -0,0 +1,164 @@
+<script>
+	import Step from '../components/Step.svelte';
+
+	let steps = [
+		{ name: 'First Project', icon: 'fa-solid fa-cart-shopping' },
+		{ name: 'Second Project', icon: 'fa-solid fa-list-check' },
+		{ name: 'Third Project', icon: 'fa-solid fa-diagram-project' }
+	];
+	let benifits = [
+		{ name: 'Self-motivated', desc: 'Self-motivated desc' },
+		{ name: 'Commited and Achiever', desc: 'Commited and Achiever desc' },
+		{ name: 'Collaborator', desc: 'Collaborator desc' }
+	];
+</script>
+
+<main class="flex flex-col flex-1 p-4">
+	<section id="introPage" class="grid grid-cols-1 lg-grd-cols-2 gap-10 py-8 sm:py-14">
+		<div class="flex flex-col lg:jusify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
+			<h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+				Hi! I'm <span class="poppins text-violet-400">Yazan</span> Ksibeh <br />Full Stack
+				<span class="poppins text-violet-400">Developer</span>
+			</h2>
+			<p class="text-base sm:text-lg md:text-xl">
+				<span class="poppins text-violet-400">Tech</span>: React.js - Sveltekit - Node.js -
+				Express.js
+			</p>
+			<a
+				href="https://www.linkedin.com/in/yazan-ksibeh/"
+				class="blueshadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer"
+				target="_blank"
+			>
+				<div
+					class="absolute top-0 right-full w-full bg-voilet-400 opacity-20 group-hover:translate-x-full z-0 duration:200"
+				/>
+				<h4 class="relative z-9">Get in Touch &rarr;</h4>
+			</a>
+		</div>
+		<div class="relative shadow-2xl grid place-items-center">
+			<img src={'images/profile.png'} alt="profile" class="h-36 rounded-full" />
+		</div>
+	</section>
+	<section id="projects" class="py-20 lg:py-32 flex flex-col gap-24">
+		<div class="flex flex-col gap-2 text-center">
+			<h6 class="text-lg sm:text-xl md:text-2xl">Few of my work</h6>
+			<h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+				Curious to <span class="poppins text-violet-400">see</span> my work?
+			</h3>
+		</div>
+		<a
+			href="https://www.youtube.com/watch?v=-2UjwQzxvBQ"
+			target="_blank"
+			class="mx-auto px-4 py-2 rounded-md border border-solid border-white flex items-center gap-2 -mb-4 sm:-mb-0 -mt-10 hover:border-violet-700 duration-200"
+		>
+			<i class="fa-regular fa-circle-play" />
+			<p>Watch the video</p>
+		</a>
+		<div class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10">
+			<Step step={steps[0]}>
+				<p>
+					First project description built with <strong class="text-violet-400">Svelte.js </strong>
+				</p></Step
+			>
+			<Step step={steps[1]}>
+				<p>
+					Second project description built with <strong class="text-violet-400">Svelte.js </strong>
+				</p></Step
+			>
+			<Step step={steps[2]}>
+				<p>
+					Third project description built with <strong class="text-violet-400">Svelte.js </strong>
+				</p></Step
+			>
+		</div>
+	</section>
+	<section
+		id="about"
+		class="py-20 pt-10 lg:pt-16 lg:py-32 flex flex-col gap-16 sm:gap-20 md:gap-24 relative"
+	>
+		<div
+			class="flex flex-col gap-2 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-700
+			after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700 py-4"
+		>
+			<h6 class="text-lg sm:text-xl md:text-2xl">Want to know more?</h6>
+			<h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+				A bit <span class="poppins text-violet-400">about</span> me.
+			</h3>
+		</div>
+		<p class="mx-auto poppins font-semibold text-lg sm:text-xl md:text-2xl">I'm . . .</p>
+		<div class="flex flex-col gap-20 w-full mx-auto max-w-[800px]">
+			{#each benifits as benifit, index}
+				<div class="flex gap-6 sm:gap-8">
+					<p class="poppins text-4xl sm:text-5xl md:text-6xl text-slate-500 font-semibold">
+						0{index + 1}
+					</p>
+					<div class="flex flex-col gap-6 sm:gap-8">
+						<h3 class="text-2xl sm:text-3xl md:text-5xl">{benifit.name}</h3>
+						<p>{benifit.desc}</p>
+					</div>
+				</div>
+			{/each}
+		</div>
+		<!-- <h5 class="text-2xl sm:text-3xl font-semiboild text-center poppins">
+			The <span class="text-violet-400 poppins">full</span> package
+		</h5> -->
+		<!-- <div class="flex flex-col overflow-x-scroll gap-10 max-w-[800px] mx-auto w-full">
+			<table class="bg-white text-slate-700 rounded text-center">
+				<thead class="border-b border-solid border-slate-200">
+					<tr class="">
+						<th />
+						<th class="whitespace-nowrap p-2 px-4">Candidate #1</th>
+						<th class="whitespace-nowrap p-2 px-4">Candidate #2</th>
+						<th class="whitespace-nowrap p-2 px-4">Candidate #3</th>
+						<th class="whitespace-nowrap bg-violet-700 text-white p-4 px-8">Me</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr class="border-b border-solid border-slate-200">
+						<td
+							class="border-r border-solid border-white pl-4 pr-8 font-semibold text-sm whitespace-nowrap"
+							>Dedication</td
+						>
+						<td><i class="fa-solid fa-xmark text-slate-500" /></td>
+						<td><i class="fa-solid fa-xmark text-slate-500" /></td>
+						<td><i class="fa-solid fa-xmark text-slate-500" /></td>
+						<td><i class="fa-solid fa-check text-green-500" /></td>
+					</tr>
+					<tr class="border-b border-solid border-slate-200">
+						<td
+							class="border-r border-solid border-white pl-4 pr-8 font-semibold text-sm whitespace-nowrap"
+							>Collaboration</td
+						>
+						<td><i class="fa-solid fa-xmark text-slate-500" /></td>
+						<td><i class="fa-solid fa-xmark text-slate-500" /></td>
+						<td><i class="fa-solid fa-xmark text-slate-500" /></td>
+						<td><i class="fa-solid fa-check text-green-500" /></td>
+					</tr>
+					<tr class="border-b border-solid border-slate-200">
+						<td
+							class="border-r border-solid border-white pl-4 pr-8 font-semibold text-sm whitespace-nowrap"
+							>Communication</td
+						>
+						<td><i class="fa-solid fa-xmark text-slate-500" /></td>
+						<td><i class="fa-solid fa-xmark text-slate-500" /></td>
+						<td><i class="fa-solid fa-xmark text-slate-500" /></td>
+						<td><i class="fa-solid fa-check text-green-500" /></td>
+					</tr>
+					<tr class="border-b border-solid border-slate-200">
+						<td
+							class="border-r border-solid border-white pl-4 pr-8 font-semibold text-sm whitespace-nowrap"
+							>Commitment</td
+						>
+						<td><i class="fa-solid fa-xmark text-slate-500" /></td>
+						<td><i class="fa-solid fa-xmark text-slate-500" /></td>
+						<td><i class="fa-solid fa-xmark text-slate-500" /></td>
+						<td><i class="fa-solid fa-check text-green-500" /></td>
+					</tr>
+				</tbody>
+			</table>
+		</div> -->
+		<div class="mx-auto -mt-12 italic sm:hidden opacity-50">
+			<p>Scroll to see more &rarr;</p>
+		</div>
+	</section>
+</main>
